33a8edc02869d1e626bea56b20c3b5eee0a2c9a9
Siftab <siiffuuu@gmail.com>
2024-08-01 22:55:32 +0600

    branch fetures

--- giitttoo.js
+++ giitttoo.js
@@ -3,6 +3,7 @@
 const { execSync } = require('child_process');
 
 const commitMessage = process.argv[2];
+const branch = process.argv[3] || 'main';
 
 if (!commitMessage) {
   console.error('You need to provide a commit message.');
@@ -10,41 +11,26 @@ if (!commitMessage) {
 }
 
 try {
-  // Execute the git commands synchronously
-  const result = execSync(`git add . && git commit -m "${commitMessage}" && git push`, { encoding: 'utf-8' });
 
-  // Log the output from Git
+
+  // Shifting branch on demand\
+  if(branch){
+    execSync(`git checkout ${branch}`, { encoding: 'utf-8' });
+    console.log(`swicthed to ${branch}`)
+    }
+  // Execute git add
+  execSync('git add .', { encoding: 'utf-8' });
+  console.log('Files added successfully.');
+
+  // Execute git commit
+  execSync(`git commit -m "${commitMessage}"`, { encoding: 'utf-8' });
+  console.log('Changes committed successfully.');
+
+  // Execute git push
+  const result = execSync(`git push origin ${branch}`, { encoding: 'utf-8' });
   console.log(result);
 } catch (error) {
   // Log any errors and exit with the error code
   console.error(error.stderr.toString());
   process.exit(error.status || 1);
 }
-
-
-// #!/usr/bin/env node
-
-// const { exec } = require('child_process');
-
-// const commitMessage = process.argv[2];
-
-// if (!commitMessage) {
-//   console.error('You need to provide a commit message.');
-//   process.exit(1);
-// }
-
-// exec(`git add . && git commit -m "${commitMessage}" && git push`, (error, stdout, stderr) => {
-//   if (stderr) {
-//     // Forward stderr to the console, which includes Git's default error messages
-//     console.error(stderr);
-//   }
-//   if (stdout) {
-//     // Log stdout, which includes the standard output from Git
-//     console.log(stdout);
-//   }
-//   if (error) {
-//     // Exit with the error code provided by the Git command
-//     console.log(error)
-//     process.exit(error.code);
-//   }
-// });
