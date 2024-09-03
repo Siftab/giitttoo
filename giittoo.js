9297d6d9bd5fa9750a2a0e20180a5378aa6ee81d
Siftab <siiffuuu@gmail.com>
2024-09-03 13:37:26 +0600

    commit issue

--- giittoo.js
+++ giittoo.js
@@ -6,22 +6,22 @@ import chalk from 'chalk';
 
 const args = process.argv.slice(2);
 const command = args[0];
-const repositoryUrl = args[1];
-const commitMessage = args.slice(2).join(' ') || 'Initial commit'; // Default commit message if not provided
-const branch = args[3] || 'main';
-console.log('Arguments:', args);
-console.log('Commit Message:', commitMessage);
+const commitMessage = args.slice(1).join(' ') || 'Initial commit'; // Capture all remaining arguments as commit message
+const branch = args[2] || 'main';
+
 console.log('Arguments:', args);
 console.log('Command:', command);
-console.log('Repository URL:', repositoryUrl);
 console.log('Commit Message:', commitMessage);
 console.log('Branch:', branch);
 
-
-
-if (command === 'setup' && repositoryUrl) {
-  initializeGitRepository(repositoryUrl);
-} else if (command === '-c') { // Updated to '-c' for add, commit, push
+if (command === 'setup') {
+  const repositoryUrl = args[1];
+  if (!repositoryUrl) {
+    console.error(chalk.red('Repository URL is required for setup.'));
+    process.exit(1);
+  }
+  initializeGitRepository(repositoryUrl, commitMessage);
+} else if (command === '-c') {
   performGitCommands(commitMessage, branch);
 } else {
   console.error(chalk.red('Invalid command or missing arguments.'));
